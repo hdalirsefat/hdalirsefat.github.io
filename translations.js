08e4121116b930423e11b33fb820998b203a7f02
hdalirsefat <59497207+hdalirsefat@users.noreply.github.com>
2025-11-15 13:15:29 +0330

    translations.js

--- translations.js
+++ translations.js
@@ -13,12 +13,12 @@ const translations = {
                     <br><br>
                     With a solid foundation of 11 years in engineering, I bring a unique perspective to data, allowing me to understand complex systems and perform deep root-cause analysis. I am passionate about designing end-to-end BI solutions—from data extraction (ETL) and process modeling (BPMN 2.0) to continuous performance monitoring and guiding digital transformation.`,
         "portfolioTitle": "Power BI Portfolio",
-        "salesTitle": "Sales Dashboard",
-        "salesDesc": "An advanced dashboard built with Power BI and DAX, analyzing sales patterns and implementing key KPIs for performance monitoring. This report provides actionable insights for senior management.",
-        "salesCaption": "Click to open the interactive report in a new tab.",
-        "hrTitle": "HR Dashboard",
-        "hrDesc": "This dashboard demonstrates data cleansing (ETL) and analysis using Power Query and SQL. It tracks critical HR metrics and provides data-driven recommendations for process improvement.",
-        "hrCaption": "Click to open the interactive report in a new tab.",
+        "DailyReportsTitle": "DailyReports Dashboard",
+        "DailyReportsDesc": "The advanced report built with Power BI and DAX on a Microsoft Project Server database compares Enterprise-level MSP plans with logged reports in SharePoint after data cleaning (ETL), and displays production-related KPIs in a dashboard with dynamic performance measurement capabilities. This report also delivers operational insights for senior management.",
+        "DailyReportsCaption": "Click to open the interactive report in a new tab.",
+        "OnlineSuperMarketTitle": "Online SuperMarket Analysis",
+        "OnlineSuperMarketDesc": "In this dashboard, data cleaning (ETL) has been performed using Power Query, and the analytical charts track the performance metrics and comparisons of supermarkets from an online service while providing data-driven recommendations.",
+        "OnlineSuperMarketCaption": "Click to open the interactive report in a new tab.",
         "resumeTitle": "My Resume",
         "resumeDesc": "Download my resume to see my qualifications, work experience, and technical skills.",
         "resumeBtnEn": "Download Resume (EN)",
@@ -27,7 +27,7 @@ const translations = {
     },
     "fa": {
         "pageTitle": "نمونه کار - هادی دلیرصفت",
-        "navTitle": "تجربیات من در تحلیل داده",
+        "navTitle": "تجربیات تحلیل داده من",
         "navHome": "خانه",
         "navAbout": "درباره من",
         "navPortfolio": "نمونه کار",
@@ -39,12 +39,12 @@ const translations = {
                     <br><br>
                     با داشتن پایه‌ای قوی از ۱۱ سال سابقه مهندسی، من دیدگاه منحصر به فردی را به دنیای داده می‌آورم که به من امکان می‌دهد سیستم‌های پیچیده را درک کنم و تحلیل عمیق ریشه‌ای (root-cause analysis) انجام دهم. من به طراحی راه‌حل‌های BI سرتاسری (end-to-end)  از استخراج داده (ETL) و مدل‌سازی فرآیند (BPMN 2.0) گرفته تا نظارت مستمر بر عملکرد و هدایت تحول دیجیتال اشتیاق دارم.`,
         "portfolioTitle": "نمونه کارهای Power BI",
-        "salesTitle": "داشبورد فروش",
-        "salesDesc": "یک داشبورد پیشرفته ساخته شده با Power BI و DAX، برای تحلیل الگوهای فروش و پیاده‌سازی KPIها. این گزارش بینش‌های عملیاتی برای مدیریت ارشد فراهم می‌کند.",
-        "salesCaption": "برای باز کردن گزارش تعاملی در تب جدید کلیک کنید.",
-        "hrTitle": "داشبورد منابع انسانی",
-        "hrDesc": "این داشبورد پاکسازی داده (ETL) و تحلیل با Power Query و SQL را نمایش می‌دهد. معیارهای حیاتی HR را ردیابی کرده و توصیه‌های مبتنی بر داده ارائه می‌دهد.",
-        "hrCaption": "برای باز کردن گزارش تعاملی در تب جدید کلیک کنید.",
+        "DailyReportsTitle": "داشبورد گزارش زوزانه تولید",
+        "DailyReportsDesc": "گزارش پیشرفته ساخته شده با Power BI و DAX بر روی دیتابیس Microsoft Project Server که برنامه های MSP در سطح Enterprise تعریف شده را با گزارشات ثبت شده در Sharepoint پس از پاکسازی داده ها (ETL) مقایسه کرده و KPIهای مرتبط با تولید را در یک داشبورد با قابلیت های سنجش داینامیک عملکرد نمایش میدهد. همچنین این گزارش بینش‌های عملیاتی برای مدیریت ارشد فراهم می‌کند.",
+        "DailyReportsCaption": "برای باز کردن گزارش تعاملی در تب جدید کلیک کنید.",
+        "OnlineSuperMarketTitle": "داشبورد تحلیل سوپرمارکت آنلاین",
+        "OnlineSuperMarketDesc": "در این داشبورد پاکسازی داده (ETL) با Power Query انجام شده و نمودارهای تحلیلی معیارهای سنجش و مقایسه سوپر مارکت های یه سرویس آنلاین را ردیابی کرده و توصیه‌های مبتنی بر داده ارائه می‌دهد.",
+        "OnlineSuperMarketCaption": "برای باز کردن گزارش تعاملی در تب جدید کلیک کنید.",
         "resumeTitle": "رزومه من",
         "resumeDesc": "رزومه من را دانلود کنید تا صلاحیت‌ها، تجربیات کاری و مهارت‌های فنی من را ببینید.",
         "resumeBtnEn": "Download Resume (EN)",
@@ -52,3 +52,4 @@ const translations = {
         "footerText": "© 2025 - هادی دلیرصفت. تمام حقوق محفوظ است."
     }
 };
+
